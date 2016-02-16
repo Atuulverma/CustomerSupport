dc9b554dede2cd21265618fbaee58484b8f1d57d
ATUL <ATUL>
2016-02-16 12:24:04 +0530

    Ever growing Customer Support Collaborative Modules

---
+++ client/controller/customers.js
@@ -0,0 +1,46 @@
+var myApp = angular.module("myApp");
+
+myApp.controller('CustomerController', ['$scope', '$http','$location', '$routeParams', function($scope, $http, $location, $routeParams){
+	console.log('Dashboard Controller Initialized...');
+
+  $scope.getCustomers = function(){
+    $http.get('/api/customers').success(function(response){
+      $scope.customers = response;
+    });
+  }
+
+	$scope.getCustomer = function(){
+		var id = $routeParams.id;
+		$http.get('/api/customers/'+id).success(function(response){
+      $scope.customer = response;
+    });
+  }
+
+	//Get customer invoices on customer detail page
+	$scope.getCustomerInvoices = function(){
+		var id = $routeParams.id;
+		$http.get('/api/invoices/customer/'+id).success(function(response){
+			$scope.customer_invoices = response;
+		});
+	}
+
+
+	$scope.addCustomer = function(){
+			$http.post('/api/customers/', $scope.customer).success(function(response){
+			window.location.href = '/#customers';
+		});
+	}
+
+	$scope.updateCustomer = function(){
+			$http.put('/api/customers/'+$scope.customer._id, $scope.customer).success(function(response){
+			window.location.href = '/#customers';
+		});
+	}
+
+	$scope.deleteCustomer = function(id){
+			$http.delete('/api/customers/'+id).success(function(response){
+			window.location.href = '/#customers';
+		});
+	}
+
+}]);
